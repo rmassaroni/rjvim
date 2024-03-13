2db66100cf89a7a2d9d6070bbf30ae18dfc47ca9
rjm <robjmass@gmail.com>
2024-03-13 15:14:15 -0400

    unnamed commit

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,6 +4,12 @@ return {
 --    opts = {},
     config = function ()
         require('lualine').setup {
+            options = {
+                disabled_filetypes = {     -- Filetypes to disable lualine for.
+                    statusline = {},       -- only ignores the ft for statusline.
+                    winbar = {},           -- only ignores the ft for winbar.
+                },
+            },
             sections = {
                 lualine_x = {'searchcount', 'fileformat', 'filesize', 'filetype'},
             }
