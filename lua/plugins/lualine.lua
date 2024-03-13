28893095736b83f4a53446ece1f97dc72d33df71
rjm <robjmass@gmail.com>
2024-03-12 22:59:28 -0400

    lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,12 @@
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = {'nvim-tree/nvim-web-devicons'},
-    opts = {},
+--    opts = {},
+    config = function ()
+        require('lualine').setup {
+            sections = {
+                lualine_x = {'searchcount', 'fileformat', 'filesize', 'filetype'},
+            }
+        }
+    end,
 }
