8b055b3feedf32ff9d0d5a06bff0903dba3e0fa9
rjm <robjmass@gmail.com>
2024-03-13 20:14:26 -0400

    local nvim-4mat

--- lua/plugins/nvim-4mat.lua
+++ lua/plugins/nvim-4mat.lua
@@ -1,4 +1,11 @@
 return {
-    --"/mnt/c/Users/12012/dev/env/rjplugs/nvim-4mat",
-    "rmassaroni/nvim-4mat"
+    {dir = "/mnt/c/Users/12012/dev/env/rjplugs/nvim-4mat"}
+--[[    {
+        "rmassaroni/nvim-4mat",
+        dependencies = {
+            "stepelu/lua-ljsqlite3",
+            "stepelu/lua-xsys"
+        }
+    }
+--]]
 }
