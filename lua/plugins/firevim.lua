e9b45e39366dff3bc5abddd5c7b33ef27ab574bd
rjm <robjmass@gmail.com>
2024-04-02 20:25:02 -0400

    firenvim fix

--- lua/plugins/firevim.lua
+++ lua/plugins/firevim.lua
@@ -3,8 +3,23 @@ return {
 
     -- Lazy load firenvim
     -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+    --cond = not not vim.g.started_by_firenvim,
     lazy = not vim.g.started_by_firenvim,
+    --lazy = false,
+    --module = false,
     build = function()
+        --require("lazy").load({ plugins = "firenvim", wait = true })
         vim.fn["firenvim#install"](0)
+    end,
+--[[
+    config = function()
+        vim.g.firenvim_config = {
+            localSettings = {
+                [".*"] = {
+                    takeover = "never",
+                },
+            },
+        }
     end
+    --]]
 }
