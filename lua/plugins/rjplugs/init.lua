a6c2cb8c6e3c94d8cb6fcc05779e5b961414f1e2
rjm <robjmass@gmail.com>
2024-04-05 14:41:48 -0400

    nvim-funs

--- lua/plugins/rjplugs/init.lua
+++ lua/plugins/rjplugs/init.lua
@@ -1,5 +1,6 @@
 return {
     {dir = "/mnt/c/Users/12012/dev/env/rjplugs/rjnotes"},
     {dir = "/mnt/c/Users/12012/dev/env/rjplugs/nvim-4mat"},
-    {dir = "/mnt/c/Users/12012/dev/env/rjplugs/invis-notes"}
+    {dir = "/mnt/c/Users/12012/dev/env/rjplugs/invis-notes"},
+    {dir = "/mnt/c/Users/12012/dev/env/rjplugs/nvim-funs"}
 }
