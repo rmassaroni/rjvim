35a3fea2628835f2f74b298f714a90e8c7046893
rjm <robjmass@gmail.com>
2024-04-06 15:27:21 -0400

    nvim-gpush config

--- lua/plugins/nvim-gpush.lua
+++ lua/plugins/nvim-gpush.lua
@@ -1,13 +1,10 @@
 return {
     "rmassaroni/nvim-gpush",
 
-    opts = {
-        one_liner = false
-    },
     config = function ()
-        local gp = require("nvim-gpush")
-        gp.setup({
-            one_liner = false
+        require("nvim-gpush").setup({
+            one_liner = true,
+            default_commit_message = "default commit"
         })
     end
 }
