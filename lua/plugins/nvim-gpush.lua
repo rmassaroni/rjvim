6105b71ba530b70be926a911ad6fd8c667bccb7a
rjm <robjmass@gmail.com>
2024-04-18 14:35:47 -0400

    nvim-gpush stuff

--- lua/plugins/nvim-gpush.lua
+++ lua/plugins/nvim-gpush.lua
@@ -4,7 +4,16 @@ return {
     config = function ()
         require("nvim-gpush").setup({
             one_liner = true,
-            default_commit_message = "default commit"
+            default_commit_message = "unnamed commit",
+            debug_mode = true,
+            file_specifc_configs = {
+                lua = {
+                    default_commit_message = "lua test"
+                },
+                java = {
+                    default_commit_message = "java test"
+                }
+            }
         })
     end
 }
