43d833e2d256c48cfaa3d50f0059c297e5ce4b52
rjm <robjmass@gmail.com>
2024-07-02 11:22:00 -0400

    auto format on yank and paste

--- lua/plugins/autotag.lua
+++ lua/plugins/autotag.lua
@@ -7,13 +7,23 @@ return {
                 -- Defaults
                 enable_close = true, -- Auto close tags
                 enable_rename = true, -- Auto rename pairs of tags
-                enable_close_on_slash = false -- Auto close on trailing </
+                enable_close_on_slash = true -- Auto close on trailing </
             },
             -- Also override individual filetype configs, these take priority.
             -- Empty by default, useful if one of the "opts" global settings
             -- doesn't work well in a specific filetype
             per_filetype = {
                 ["html"] = {
+                    enable_close = true
+                },
+                ["tsx"] = {
+                    enable_close = true,
+                    enable_rename = false
+                },
+                ["javascript"] = {
+                    enable_close = true
+                },
+                ["typescript"] = {
                     enable_close = true
                 }
             }
