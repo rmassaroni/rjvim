9a76dfe1c8ed9c08d22eae7465739e772d9bd470
rmassaroni <rmassaro@villanova.edu>
2024-08-29 13:52:24 -0400

    unfinished alpha.lua

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,27 @@
+return {
+   	"goolord/alpha-nvim",
+    config = function ()
+        local alpha = require'alpha'
+        local dashboard = require'alpha.themes.dashboard'
+ 	    -- dashboard.section.header.val = require("plugins.alpha.rjvim")
+	    dashboard.section.buttons.val = {
+--        	    dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+--             dashboard.button("p", "P  New project", ":<CR>"),
+-- --            dashboard.button("s", "S  Script", ":e ~/.bashrc<CR>"),
+--             dashboard.button("t", "ℸ  Open tree", ":NvimTreeOpen<CR>"),
+-- 	 	    dashboard.button("c", "ᓵ  Config", ":e $HOME/.config/nvim/init.lua<CR> :cd $HOME/.config/nvim/lua/plugins<CR> :NvimTreeOpen<CR>"),
+            dashboard.button("l", "ꖎ  Lazy", ":Lazy<CR>"),
+--             dashboard.button("m", "ᒲ  Mason", ":Mason<CR>"),
+--     	    dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
+            --dashboard.button("<Right>", "Toggle", reload_dashboard)
+        }
+      	--local handle = io.popen('fortune')
+ 		--local fortune = handle:read("*a")
+  		--handle:close()
+ 		--dashboard.section.footer.val = fortune
+        dashboard.section.footer.val = vim.g.colors_name
+  	    dashboard.config.opts.noautocmd = true
+     	--vim.cmd[[autocmd User AlphaReady echo 'ready']]
+ 	    alpha.setup(dashboard.config)
+	end
+}
