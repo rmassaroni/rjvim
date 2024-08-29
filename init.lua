2421b3e48e53bc3b046d396b5acc702e99938c8b
rmassaroni <rmassaro@villanova.edu>
2024-08-29 13:21:20 -0400

    restart

--- init.lua
+++ init.lua
@@ -41,47 +41,47 @@ vim.opt.clipboard:append("unnamedplus")
 
 -------LAZY SETUP
 -- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-local lazypath = "C:\\Program Files\\nvim-win64\\bin\\lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-require("lazy").setup({
-    spec = {import = "plugins"},
-    ui = {
-        border = "double",
-        size = {
-            width = 0.8,
-            height = 0.8,
-        },
-    },
-    checker = {
-        enabled = true,
-        notify = false,
-    },
-    change_detection = {notify = false},
-})
-
-require'nvim-treesitter.configs'.setup {
-    auto_install = true,
-  --ensure_installed = 'all', -- Install all available parsers
-  highlight = {
-    enable = true, -- Enable Treesitter's syntax highlighting
-  },
-  incremental_selection = {
-    enable = true, -- Enable Treesitter's incremental selection
-  },
-  -- Disable other parsers
-  parser = {
-    enabled = { 'treesitter' }, -- Only enable the Treesitter parser
-    disable_other = true, -- Disable all other parsers
-  },
-}
+-- local lazypath = "C:\\Program Files\\nvim-win64\\bin\\lazy.nvim"
+-- if not vim.loop.fs_stat(lazypath) then
+--   vim.fn.system({
+--     "git",
+--     "clone",
+--     "--filter=blob:none",
+--     "https://github.com/folke/lazy.nvim.git",
+--     "--branch=stable",
+--     lazypath,
+--   })
+-- end
+-- vim.opt.rtp:prepend(lazypath)
+-- require("lazy").setup({
+--     spec = {import = "plugins"},
+--     ui = {
+--         border = "double",
+--         size = {
+--             width = 0.8,
+--             height = 0.8,
+--         },
+--     },
+--     checker = {
+--         enabled = true,
+--         notify = false,
+--     },
+--     change_detection = {notify = false},
+-- })
+--
+-- require'nvim-treesitter.configs'.setup {
+--     auto_install = true,
+--   --ensure_installed = 'all', -- Install all available parsers
+--   highlight = {
+--     enable = true, -- Enable Treesitter's syntax highlighting
+--   },
+--   incremental_selection = {
+--     enable = true, -- Enable Treesitter's incremental selection
+--   },
+--   -- Disable other parsers
+--   parser = {
+--     enabled = { 'treesitter' }, -- Only enable the Treesitter parser
+--     disable_other = true, -- Disable all other parsers
+--   },
+-- }
 
