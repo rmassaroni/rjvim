e5f7a031465eb00e185868cf2daac85ea9055b24
rmassaroni <rmassaro@villanova.edu>
2024-08-29 13:54:18 -0400

    nvimtree.lua

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -8,11 +8,11 @@ return {
 --        	    dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
 --             dashboard.button("p", "P  New project", ":<CR>"),
 -- --            dashboard.button("s", "S  Script", ":e ~/.bashrc<CR>"),
---             dashboard.button("t", "ℸ  Open tree", ":NvimTreeOpen<CR>"),
+            dashboard.button("t", "ℸ  Open tree", ":NvimTreeOpen<CR>"),
 -- 	 	    dashboard.button("c", "ᓵ  Config", ":e $HOME/.config/nvim/init.lua<CR> :cd $HOME/.config/nvim/lua/plugins<CR> :NvimTreeOpen<CR>"),
             dashboard.button("l", "ꖎ  Lazy", ":Lazy<CR>"),
 --             dashboard.button("m", "ᒲ  Mason", ":Mason<CR>"),
---     	    dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
+    	    dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
             --dashboard.button("<Right>", "Toggle", reload_dashboard)
         }
       	--local handle = io.popen('fortune')
