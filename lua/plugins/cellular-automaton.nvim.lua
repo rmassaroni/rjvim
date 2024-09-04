9b1eb1a1feec661535497e81296bfa809e053526
rmassaroni <rmassaro@villanova.edu>
2024-09-04 11:09:30 -0400

    cellular-automaton.nvim

---
+++ lua/plugins/cellular-automaton.nvim.lua
@@ -0,0 +1,16 @@
+return {
+    "eandrju/cellular-automaton.nvim",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+    },
+    config = function ()
+        local configs = require("nvim-treesitter.configs")
+        configs.setup({
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true }
+        })
+    end,
+    vim.keymap.set('n', '<leader>c',':CellularAutomaton make_it_rain<CR>', {noremap = true})
+}
