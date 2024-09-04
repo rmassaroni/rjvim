47c57c1a5e09f0f770e3daff026b599852ab8152
rmassaroni <rmassaro@villanova.edu>
2024-09-04 11:33:49 -0400

    nvim-cmp

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,5 +1,6 @@
 return {
     --require("plugins/lsp/neodev"),
     require('plugins.lsp.mason'),
-    require('plugins.lsp.nvim-lspconfig')
+    require('plugins.lsp.nvim-lspconfig'),
+    require('plugins.lsp.nvim-cmp')
 }
