5de195ad3d8f37a7563f3050cef2c710984e2e91
rmassaroni <rmassaro@villanova.edu>
2024-09-17 14:02:33 -0400

    cleanup

--- lua/lazysetup.lua
+++ lua/lazysetup.lua
@@ -6,23 +6,24 @@ if not vim.loop.fs_stat(lazypath) then
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable",
-    lazypath,
+    lazypath
   })
 end
+
 vim.opt.rtp:prepend(lazypath)
+
 require("lazy").setup({
     spec = {import = "plugins"},
-    -- spec = {},
     ui = {
         border = "double",
         size = {
             width = 0.8,
-            height = 0.8,
-        },
+            height = 0.8
+        }
     },
     checker = {
         enabled = true,
         notify = false,
     },
-    change_detection = {notify = false},
+    change_detection = {notify = false}
 })
