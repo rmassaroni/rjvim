9fe6c8e3cc6b981899e7f4fa2cc9c51141eabade
rmassaroni <robjmass@gmail.com>
2025-02-04 10:54:44 -0500

    enchantify attempt

---
+++ lua/plugins/enchantify.lua
@@ -0,0 +1,8 @@
+return {
+    -- {dir = "~/projects/enchantify",
+    -- config = function()
+    --     -- local enchant = require("enchantify")
+    --     -- vim.api.nvim_create_user_command("Enchantify", enchant.enchant_current_buffer, {})
+    -- end
+    -- }
+}
